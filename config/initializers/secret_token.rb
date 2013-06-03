e8e726ca1c64ba6c66f39ed4448b285ac3c75deb
Anderson Campos de Mendonça <andersoncampos.m@gmail.com>
2013-06-03 15:12:37 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rvp::Application.config.secret_token = '23461b13459b226e14198f038981aea9a05b3806ae74021443dcdc53f5efcab6bffc4ed0a5350b5204afd2596dd80f2513b780fbbf37da520d339a492f6862d8'
